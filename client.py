ccf08694501a70b7863d65f8d6d5a13a4dae9db6
OlenaSkakun-IPZ-22 <olena_skakunn@knu.ua>
2025-06-26 16:54:59 +0300

    change client/py

--- client.py
+++ client.py
@@ -40,7 +40,7 @@ if uploaded_files:
             with st.spinner("Обробка файлів..."):
                 files_for_request = [("files", (f.name, f.getvalue(), f.type)) for f in uploaded_files]
                 try:
-                    response = requests.post("http://192.168.0.104:5000/upload", files=files_for_request)
+                    response = requests.post("https://назва-додатку.onrender.com/upload", files=files_for_request)
                     
                     if response.status_code == 200:
                         results = response.json()
